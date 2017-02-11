b57c093c4142150afc490fb7ccb3fd84d99d00f5
zhang <zhangzhengyi12@live.com>
2017-02-12 00:04:55 +0800

    complate AD Carousel

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by zhang on 2017/2/11.
+ */
+
+$(function () {
+
+    function resize() {
+        var windowWidth = $(window).width();
+        //获取屏幕的宽度
+        var isSmallScreen = windowWidth < 768;
+        //判断是否是小屏幕
+        $('#carousel-example-generic > .carousel-inner > .item').each(function(i, item) {
+            var $item = $(item);
+            var src = ($item.data(isSmallScreen ? 'image-sm' : 'image-lg'));
+            $item.css('backgroundImage', 'url("' + src + '")');
+            if (isSmallScreen) {
+                $item.html('<img src="' + src + '" alt="" />');
+            } else {
+                $item.empty();
+            }
+        })
+        //获取一个存放item的数组
+    }
+    $(window).on('resize',resize).trigger('resize');
+})
