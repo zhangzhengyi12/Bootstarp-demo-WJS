dcf8b2d86af722253eabebe599247410af7ab34d
zhang <zhangzhengyi12@live.com>
2017-02-18 01:02:08 +0800

    comlate modo  and mobile gallery

--- src/js/main.js
+++ src/js/main.js
@@ -55,6 +55,38 @@ $(function () {
     //     }
     // })
 
+
+//    关于轮播图触摸支持的实现
+//    首先找到在轮播图上X轴的移动 然后做出响应的动作
+    var $carousels = $('.carousel');
+    var startx ,endx;
+    var dis = 80;
+    //获取触摸参数
+    $carousels.on('touchstart',function (e) {
+        startx = e.originalEvent.touches[0].clientX
+    })
+    //获取开始的值
+
+    $carousels.on('touchmove',function (e) {
+        endx = e.originalEvent.touches[0].clientX
+    })
+    //通过变量不断的覆盖value 获取到最后一刻的x值
+    $carousels.on('touchend',function (e) {
+        if(Math.abs(startx - endx) > dis){
+            // startx > endx ? $carousels.carousel('next') : $carousels.carousel('prev');
+
+            // if(startx > endx ){
+            //     $carousels.carousel('next');
+            // }else if(startx < endx ){
+            //     $carousels.carousel('prev');
+            // }
+            //
+
+            startx > endx ? $carousels.carousel('next') : startx < endx ? $carousels.carousel('prev') : 1 ;
+        }
+
+    }) //在触摸完成后 触发对比事件 和bootstarp自带的翻页方法
+
 });
 
 // numWidth > $(window).width()
